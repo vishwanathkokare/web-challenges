260727a35ea4c17b8392480d0e1fb0e9c50d08ba
vishwanathkokare <G0dB3liv3r+github@protonmail.com>
2024-04-10 11:51:33 +0000

    FIX: rate issue of indexing

--- frontendmentor/interactive-rating-component/script.js
+++ frontendmentor/interactive-rating-component/script.js
@@ -4,13 +4,12 @@ const card = document.querySelector('.card');
 const thankYou = document.querySelector('.thankyou');
 const rate = document.querySelector('.rate')
 let clicked = false;
-let rate_value;
+let rate_value = 1;
 
 const removeAll = () =>{
     btns.forEach(item=>item.classList.remove('clicked'));
     clicked = false;
-    rate_value = undefined;
-    console.log('removed',clicked);
+    rate_value = 1;
 }
 
  btns.forEach((item,index)=>{
@@ -18,12 +17,11 @@ const removeAll = () =>{
     if(!clicked){
         item.classList.add('clicked');
         clicked = true;
-        rate_value = index;
-        console.log('clicked',index,clicked)
+        rate_value += index;
    }else{
         removeAll();  
         item.classList.add('clicked');
-        rate_value = index;
+        rate_value += index;
         clicked = true;
     }
    }) 
@@ -34,5 +32,7 @@ subBtn.addEventListener('click',()=>{
        card.style.display = 'none';
        thankYou.style.display = 'block';
        rate.innerHTML = rate_value;
+    }else{
+        
     }
 })
